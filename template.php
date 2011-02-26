9a60dc7cd166e40bdf063b289bb84cfb6a64e860
John Sawers <john@johnksawers.com>
2011-02-25 23:36:58 -0500

    massive re jiggering to be an AdaptiveTheme subtheme

--- template.php
+++ template.php
@@ -1,150 +1,98 @@
 <?php
-// $Id: template.php,v 1.1 2009/05/15 07:28:05 agileware Exp $
-
-require_once("common_methods.php");
-
-function get_full_path_to_theme() {
-  return base_path() . path_to_theme();
-}
-
-function earthen_service_links_node_format($links) {
-  return '<div class="service-links"><div class="service-label">'. t('Bookmark/Search this post with: ') .'</div>'. art_links_woker($links) .'</div>';
-}
+// $Id: template.php,v 1.2.4.8 2010/12/18 06:35:00 jmburnz Exp $
 
 /**
- * Generate the HTML output for a single local task link.
- *
- * @ingroup themeable
+ * Add the Style Schemes if enabled.
+ * NOTE: You MUST make changes in your subthemes theme-settings.php file
+ * also to enable Style Schemes.
  */
-function earthen_menu_local_task($link, $active = FALSE) {
-  $output = preg_replace('~<a href="([^"]*)"[^>]*>([^<]*)</a>~',
-  '<a href="$1" class="Button">'
-  .'<span class="btn">'
-  .'<span class="l"></span>'
-  .'<span class="r"></span>'
-  .'<span class="t">$2</span>'
-  .'</span>'
-  .'</a>', $link);
-  return $output;
+/* -- Delete this line if you want to enable style schemes.
+// DONT TOUCH THIS STUFF...
+function get_at_styles() {
+  $scheme = theme_get_setting('style_schemes');
+  if (!$scheme) {
+    $scheme = 'style-default.css';
+  }
+  if (isset($_COOKIE["atstyles"])) {
+    $scheme = $_COOKIE["atstyles"];
+  }
+  return $scheme;
 }
-
-function earthen_breadcrumb($breadcrumb) {
-  $breadcrumb = menu_get_active_breadcrumb();
-  if (!empty($breadcrumb)) {
-    return '<div class="breadcrumb">' . implode(' | ', $breadcrumb) . '</div>';
+if (theme_get_setting('style_enable_schemes') == 'on') {
+  $style = get_at_styles();
+  if ($style != 'none') {
+    drupal_add_css(path_to_theme() . '/css/schemes/' . $style, array('group' => CSS_THEME, 'preprocess' => TRUE));
   }
 }
-
-function earthen_comment_wrapper($content, $type = null) {
-  static $node_type;
-  if (isset($type)) $node_type = $type;
-  return '<div id="comments">'. $content . '</div>';
-}
-
-function earthen_menu_local_tasks() {
-  $output = '';
-
-  if ($primary = menu_primary_local_tasks()) {
-    $output .= $primary;
-  }
-  if ($secondary = menu_secondary_local_tasks()) {
-    $output .= $secondary;
-  }
-  return $output;
-}
+// */
 
 /**
- * Theme a form button.
- *
- * @ingroup themeable
+ * Preprocess and Process Functions SEE: http://drupal.org/node/254940#variables-processor
+ * 1. Rename each function to match your subthemes name,
+ *    e.g. if you name your theme "themeName" then the function
+ *    name will be "themeName_preprocess_hook". Tip - you can
+ *    search/replace on "adaptivetheme_subtheme".
+ * 2. Uncomment the required function to use.
  */
-function earthen_button($element) {
-  // Make sure not to overwrite classes.
-  if (isset($element['#attributes']['class'])) {
-    $element['#attributes']['class'] = 'Button form-' . $element['#button_type'] . ' ' . $element['#attributes']['class'];
-  }
-  else {
-    $element['#attributes']['class'] = 'Button form-' . $element['#button_type'];
-  }
- 
-  // Skip admin pages due to some issues with ajax looking for <input> not <button>.
-  if (arg(0) == 'admin') {
-    return '<input type="submit" '. (empty($element['#name']) ? '' : 'name="'. $element['#name'] .'" ') .'id="'. $element['#id'] .'" value="'. check_plain($element['#value']) .'" '. drupal_attributes($element['#attributes']) ." />\n";
-  }
- 
-  return '<button type="submit" ' . (empty($element['#name']) ? '' : 'name="' . $element['#name']
-         . '" ')  . 'id="' . $element['#id'] . '" value="' . check_plain($element['#value']) . '" ' . drupal_attributes($element['#attributes']) . '>'
-         . '<span class="btn">'
-         . '<span class="l"></span>'
-         . '<span class="r"></span>'
-         . '<span class="t">' . check_plain($element['#value']) . '</span>'
-         . '</span></button>';
-}
 
 /**
- * Image assist module support.
- * Using Artisteer styles in IE
-*/
-function earthen_img_assist_page($content, $attributes = NULL) {
-  $title = drupal_get_title();
-  $output = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">'."\n";
-  $output .= '<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">'."\n";
-  $output .= "<head>\n";
-  $output .= '<title>'. $title ."</title>\n";
-
-  // Note on CSS files from Benjamin Shell:
-  // Stylesheets are a problem with image assist. Image assist works great as a
-  // TinyMCE plugin, so I want it to LOOK like a TinyMCE plugin. However, it's
-  // not always a TinyMCE plugin, so then it should like a themed Drupal page.
-  // Advanced users will be able to customize everything, even TinyMCE, so I'm
-  // more concerned about everyone else. TinyMCE looks great out-of-the-box so I
-  // want image assist to look great as well. My solution to this problem is as
-  // follows:
-  // If this image assist window was loaded from TinyMCE, then include the
-  // TinyMCE popups_css file (configurable with the initialization string on the
-  // page that loaded TinyMCE). Otherwise, load drupal.css and the theme's
-  // styles. This still leaves out sites that allow users to use the TinyMCE
-  // plugin AND the Add Image link (visibility of this link is now a setting).
-  // However, on my site I turned off the text link since I use TinyMCE. I think
-  // it would confuse users to have an Add Images link AND a button on the
-  // TinyMCE toolbar.
-  // 
-  // Note that in both cases the img_assist.css file is loaded last. This
-  // provides a way to make style changes to img_assist independently of how it
-  // was loaded.
-  $output .= drupal_get_html_head();
-  $output .= drupal_get_js();
-  $output .= "\n<script type=\"text/javascript\"><!-- \n";
-  $output .= "  if (parent.tinyMCE && parent.tinyMCEPopup && parent.tinyMCEPopup.getParam('popups_css')) {\n";
-  $output .= "    document.write('<link href=\"' + parent.tinyMCEPopup.getParam('popups_css') + '\" rel=\"stylesheet\" type=\"text/css\">');\n";
-  $output .= "  } else {\n";
-  foreach (drupal_add_css() as $media => $type) {
-    $paths = array_merge($type['module'], $type['theme']);
-    foreach (array_keys($paths) as $path) {
-      // Don't import img_assist.css twice.
-      if (!strstr($path, 'img_assist.css')) {
-        $output .= "  document.write('<style type=\"text/css\" media=\"{$media}\">@import \"". base_path() . $path ."\";<\/style>');\n";
-      }
-    }
-  }
-  $output .= "  }\n";
-  $output .= "--></script>\n";
-  // Ensure that img_assist.js is imported last.
-  $path = drupal_get_path('module', 'img_assist') .'/img_assist_popup.css';
-  $output .= "<style type=\"text/css\" media=\"all\">@import \"". base_path() . $path ."\";</style>\n";
-
-  $output .= '<!--[if IE 6]><link rel="stylesheet" href="'.get_full_path_to_theme().'/style.ie6.css" type="text/css" /><![endif]-->'."\n";
-  $output .= '<!--[if IE 7]><link rel="stylesheet" href="'.get_full_path_to_theme().'/style.ie7.css" type="text/css" /><![endif]-->'."\n";
-
-  $output .= "</head>\n";
-  $output .= '<body'. drupal_attributes($attributes) .">\n";
-
-  $output .= theme_status_messages();
-
-  $output .= "\n";
-  $output .= $content;
-  $output .= "\n";
-  $output .= '</body>';
-  $output .= '</html>';
-  return $output;
+ * Override or insert variables into all templates.
+ */
+/* -- Delete this line if you want to use these functions
+function adaptivetheme_subtheme_preprocess(&$vars, $hook) {
 }
+function adaptivetheme_subtheme_process(&$vars, $hook) {
+}
+// */
+
+/**
+ * Override or insert variables into the html templates.
+ */
+/* -- Delete this line if you want to use these functions
+function adaptivetheme_subtheme_preprocess_html(&$vars) {
+  // Uncomment the folowing line to add a conditional stylesheet for IE 7 or less.
+  // drupal_add_css(path_to_theme() . '/css/ie/ie-lte-7.css', array('weight' => CSS_THEME, 'browsers' => array('IE' => 'lte IE 7', '!IE' => FALSE), 'preprocess' => FALSE));
+}
+function adaptivetheme_subtheme_process_html(&$vars) {
+}
+// */
+
+/**
+ * Override or insert variables into the page templates.
+ */
+/* -- Delete this line if you want to use these functions
+function adaptivetheme_subtheme_preprocess_page(&$vars) {
+}
+function adaptivetheme_subtheme_process_page(&$vars) {
+}
+// */
+
+/**
+ * Override or insert variables into the node templates.
+ */
+/* -- Delete this line if you want to use these functions
+function adaptivetheme_subtheme_preprocess_node(&$vars) {
+}
+function adaptivetheme_subtheme_process_node(&$vars) {
+}
+// */
+
+/**
+ * Override or insert variables into the comment templates.
+ */
+/* -- Delete this line if you want to use these functions
+function adaptivetheme_subtheme_preprocess_comment(&$vars) {
+}
+function adaptivetheme_subtheme_process_comment(&$vars) {
+}
+// */
+
+/**
+ * Override or insert variables into the block templates.
+ */
+/* -- Delete this line if you want to use these functions
+function adaptivetheme_subtheme_preprocess_block(&$vars) {
+}
+function adaptivetheme_subtheme_process_block(&$vars) {
+}
+// */
